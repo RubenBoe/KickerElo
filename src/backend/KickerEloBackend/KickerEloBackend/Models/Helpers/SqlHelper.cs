b031a1072d58ed8680d42548f13786793dcc232e
Ruben Bösche <ruben.boesche@t-online.de>
2024-06-28 17:07:22 +0200

    Migrate data to SQL

---
+++ src/backend/KickerEloBackend/KickerEloBackend/Models/Helpers/SqlHelper.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace KickerEloBackend.Models.Helpers
+{
+    internal class SqlHelper
+    {
+        internal static SqlConnection GetSqlConnection()
+        {
+            var conn = new SqlConnection(Environment.GetEnvironmentVariable("SQL_CONNECTION_STRING"));
+            conn.Open();
+
+            return conn;
+        }
+    }
+}
---
+++ src/database/01. Base schema definitions.sql
@@ -0,0 +1,67 @@
+
+CREATE TABLE dbo.clients (
+    Id int IDENTITY(1,1) PRIMARY KEY CLUSTERED,
+    ClientName nvarchar(200) NOT NULL,
+    CreationDate DATETIME NOT NULL,
+    ClientToken NVARCHAR(200) NOT NULL
+)
+
+GO
+
+CREATE TABLE dbo.seasons (
+    SeasonID NVARCHAR(200) PRIMARY KEY CLUSTERED,
+    ClientID INT NOT NULL
+        REFERENCES dbo.clients(Id),
+    SeasonNumber INT NOT NULL,
+    StartDate DATETIME NOT NULL,
+    EndDate DATETIME NULL
+)
+
+GO
+
+CREATE TABLE dbo.players (
+    PlayerID NVARCHAR(200) PRIMARY KEY CLUSTERED,
+    ClientID INT NOT NULL
+        REFERENCES dbo.clients(Id),
+    Nickname NVARCHAR(100) NOT NULL,
+    FullName NVARCHAR(1000) NOT NULL
+)
+
+GO
+
+CREATE TABLE dbo.playerElo (
+    PlayerID NVARCHAR(200) NOT NULL
+        REFERENCES dbo.players(PlayerID),
+    SeasonID NVARCHAR(200) NOT NULL
+        REFERENCES dbo.seasons(SeasonID),
+    EloNumber int NOT NULL,
+    LastUpdated DATETIME NOT NULL,
+    CONSTRAINT UC_PlayerID_SeasonID UNIQUE (PlayerID, SeasonID)
+)
+
+GO
+
+CREATE TABLE dbo.games (
+    GameID NVARCHAR(200) PRIMARY KEY CLUSTERED,
+    SeasonID NVARCHAR(200) NOT NULL 
+        REFERENCES dbo.seasons(SeasonID),
+    ClientID int NOT NULL
+        REFERENCES dbo.clients(Id),
+    [Date] DATETIME NOT NULL DEFAULT GETUTCDATE()
+)
+
+GO
+
+CREATE TABLE dbo.playerGame (
+    GameID NVARCHAR(200) NOT NULL
+        REFERENCES dbo.games(GameID),
+    PlayerID NVARCHAR(200) NOT NULL
+        REFERENCES dbo.players(PlayerID),
+    Team INT NOT NULL,
+    Points INT NOT NULL,
+    EloGain INT NOT NULL,
+    CONSTRAINT UC_PlayerID_SeasonID UNIQUE (PlayerID, SeasonID)
+)
+
+GO
+
